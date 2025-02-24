b4fd26784a16c3a03a3b5c22aef13eaafa8610db
Ezgi Yıldız <yildizezgi617@gmail.com>
2025-02-24 16:57:53 +0300

    Hata yönetimi eklendi

--- 09-DoWhile/Program.cs
+++ 09-DoWhile/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             //koşula bakmadan önce yapılacak olan işi yapar sonra döngüye başlar.
-            string sifre;
+            //string sifre;
             //do
             //{
             //    Console.WriteLine("şifreyi girin");
@@ -15,19 +15,56 @@
             //kullanıcıdan geçerli yaş girene kadar sürekli yaş sorun. yaş 0 ile 120 arasında olmalıdır. bu aralık dışında her girişte program tekrar sormalıdır.
 
 
-            int yas;
+            //int yas;
 
-            do
+            //do
+            //{
+            //    Console.WriteLine("yaş?");
+            //    yas = int.Parse(Console.ReadLine());
+
+            //    if(yas<0 || yas > 120)
+            //    {
+            //        Console.WriteLine("geçersiz yaş");
+            //    }
+
+            //} while (yas < 0 || yas > 120);
+            //Console.WriteLine("geçerli bir yaş");
+
+            #region Kontrol Deyimleri
+            //Break, bir döngüyü veya switch yapısını anında sonlandırmak için (scope yapısının dışına tamak için) kullanılır.
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("yaş?");
-                yas = int.Parse(Console.ReadLine());
-                
-                if(yas<0 || yas > 120)
-                {
-                    Console.WriteLine("geçersiz yaş");
-                }
+                if (i == 5)
+                    break;    //5 olduğunda breake gelerek döngüyü kırar.
+                Console.WriteLine(i);
+            }
 
-            } while (yas < 0 || yas > 120);
+            //Continue, döngüyü ya da switchi bozmaz iterasyon atlatır.
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                    continue;    //5 olduğunda yazdırmaz diğer tüm değerleri yazdırır.
+                Console.WriteLine(i);
+            }
+
+            //Return değer döndürmek için kullanılır.
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                    return;    //
+                Console.WriteLine(i);
+            }
+
+            //goto
+            int sayac = 0;
+            start:                  //sayac 1 arttırıldıktan sonra buraya gelir.
+            if (sayac < 5)
+            {
+                Console.WriteLine(sayac);
+                sayac++;
+                goto start;  //döngü oluşturulmuş gibi olur. startın ilk tanımlandığı yere gider.
+            }
+            #endregion
         }
     }
 }
---
+++ 10-TryCatch/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace _10_TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+        /* Hata Yönetimi
+         *1-Derleme zamanı hataları(Compiler time errors)
+         *2- Çalışma Zamanı Hataları (Runtime Error)
+         3-Mantıksal Hatalar (Logical Errors)
+        */
+        start:
+            //try
+            //{
+            //    while (true)
+            //    {
+            //        Console.WriteLine("telefon no");
+            //        int telNo = int.Parse(Console.ReadLine());    //Hata Gelmesi Muhtemel kodlarımızı yazacağımız alan
+            //    }   
+            //}
+            //catch (Exception ex)   //hatayı oluşturdun ve ex ismini verdin
+            //{
+            //    //Hata Olduğunda Ne Yapacaksınız
+            //    Console.WriteLine("yanlış giriş yaptın");
+            //    Console.WriteLine(ex.Message);             //exin mesajını yazdırdın.
+            // goto start;
+            //}
+
+
+            //Bu çok genel bir hata yönetimiydi. Daha özelleştirebiliriz o zaman hatayı oluştururken direkt hata türünden oluştururuz.
+            //try
+            //{
+            //    while (true)
+            //    {
+            //        Console.WriteLine("telefon no");
+            //        int telNo = int.Parse(Console.ReadLine());    //Hata Gelmesi Muhtemel kodlarımızı yazacağımız alan
+            //    }
+            //}
+            //catch (OverflowException ex)   //hatayı oluşturdun ve ex ismini verdin
+            //{
+            //    //Hata Olduğunda Ne Yapacaksınız
+            //    Console.WriteLine("Girdiğiniz sayı çok büyük ya da çok küçük");
+            //    Console.WriteLine(ex.Message);             //exin mesajını yazdırdın.
+            //    goto start;
+            //}
+            //catch (FormatException ex)   //hatayı oluşturdun ve ex ismini verdin
+            //{
+            //    //Hata Olduğunda Ne Yapacaksınız
+            //    Console.WriteLine("Geçersiz giriş lütfen bir tamsayı giriniz");
+            //    Console.WriteLine(ex.Message);             //exin mesajını yazdırdın.
+            //    goto start;
+
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine( "bir hata oluştu");   //Bunu en başa yazsaydık tüm hatalarda bu mesaj dönerdi özelleştirmenin bir anlamı kalmazdı.
+            //    Console.WriteLine(ex.Message);
+            //    goto start;
+            //}
+            //finally
+            //{                                        //Finally hata gelse de gelmese de çalışır.
+
+            //    Console.WriteLine("ben hata gelse de gelmese de çalışırım");
+
+            //}
+
+            //TryParse, tip dönüşümlerinde hatalar çok olur. bu yüzden burda onun kontrolünü yapıyoruz. true ya da false dönüyor. dönüşürse out ifadesi ile belirtilen ifadeye dönüştürdüğü sayıyı yükleme yapıyor.
+            //basla:
+            //Console.WriteLine("bir sayı gir");
+            //string sayi = Console.ReadLine();
+            //int donusensayi = 0;
+
+            //bool gecerliSayi = int.TryParse(sayi,out donusensayi);
+
+            //if(!gecerliSayi) {
+            //    Console.WriteLine("geçersiz giriş yaptınız tekrar deneyiniz");
+            //    goto basla;
+            //}
+            //Console.WriteLine("Girdiğiniz sayi"+sayi);
+
+
+            //KULLANICIDAN 0 İLE 100 ARALIĞINDA DEĞER AL KONTROLÜNÜ YAP
+            //Burada hem sayı olup olmadığını hem de değer aralığını kontrol etmem lazım.
+            int not;
+            bool gecerliNot = false;
+            while (!gecerliNot)
+            {
+                Console.WriteLine("0 ile 100 arası değer giriniz");
+                string girdi = Console.ReadLine();  
+                if(int.TryParse(girdi, out not) && not >=0 && not<=100) {
+
+                    Console.WriteLine("girilen not"+not);
+                }
+            }          
+        }
+    }
+}
